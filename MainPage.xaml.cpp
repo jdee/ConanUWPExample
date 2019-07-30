f7b11ffce7ef73c8ea5ca6d9cbfa3e7cba26b9ee
Jimmy Dee <jgvdthree@gmail.com>
2019-07-30 14:57:37 -0700

    Initial commit

---
+++ MainPage.xaml.cpp
@@ -0,0 +1,38 @@
+﻿//
+// MainPage.xaml.cpp
+// Implementation of the MainPage class.
+//
+
+#include "pch.h"
+#include "MainPage.xaml.h"
+
+#include <sstream>
+#include <Poco/Environment.h>
+
+using namespace ConanUWPExample;
+
+using namespace Platform;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+MainPage::MainPage()
+{
+	InitializeComponent();
+
+	Poco::Environment env;
+	std::string osName(env.osName());
+	std::wstring wosName(osName.begin(), osName.end());
+
+	std::wostringstream woss;
+	woss << L"Hello, " << wosName << L"!";
+	outputTextBlock->Text = ref new String(woss.str().c_str());
+}
